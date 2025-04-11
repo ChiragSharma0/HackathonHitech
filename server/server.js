e79674138ff1c4b87fc13c8c1fa030e0fd0998fa
Anish pal <anishpal9667@gmail.com>
2025-04-11 12:32:11 +0530

    chat

--- server/server.js
+++ server/server.js
@@ -1,1 +1,31 @@
-hello
+
+const express = require('express');
+const http = require('http');
+const cors = require('cors');
+const { Server } = require('socket.io');
+
+const app = express();
+app.use(cors());
+
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: '*',
+  },
+});
+
+io.on('connection', (socket) => {
+  console.log('A user connected');
+
+  socket.on('send-message', (data) => {
+    io.emit('receive-message', data);
+  });
+
+  socket.on('disconnect', () => {
+    console.log('User disconnected');
+  });
+});
+
+server.listen(5000, () => {
+  console.log('Server running on http://localhost:5000');
+});
