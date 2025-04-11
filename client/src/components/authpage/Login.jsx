ab09b45aeebdd0bb8b5b9349eb74ac14a497fbf4
ChandraPrakashSingh281@gmail.com <ChandraPrakashSingh281@gmail.com>
2025-04-11 16:00:14 +0530

    login,auth,signup

--- client/src/components/authpage/Login.jsx
+++ client/src/components/authpage/Login.jsx
@@ -1,1 +1,8 @@
-f
+import React from "react";
+import AuthForm from "../components/AuthForm";
+
+const Login = () => {
+  return <AuthForm isSignup={false} />;
+};
+
+export default Login;
--- server/server.js
+++ server/server.js
@@ -1,9 +1,13 @@
-
+const dotenv = require('dotenv');
+dotenv.config();
 const express = require('express');
 const http = require('http');
 const cors = require('cors');
 const { Server } = require('socket.io');
+const authRoutes = require('./routes/authRoutes');
+const connectDB = require('./config/db');
 
+connectDB();
 const app = express();
 app.use(cors());
 
@@ -26,6 +30,8 @@ io.on('connection', (socket) => {
   });
 });
 
-server.listen(5000, () => {
-  console.log('Server running on http://localhost:5000');
+app.use("/api/auth", authRoutes);
+
+server.listen(process.env.PORT, () => {
+  console.log(`Server running on port ${process.env.PORT}`);
 });
