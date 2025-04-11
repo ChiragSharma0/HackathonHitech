aff85e5c96604f07c5e12ca82d8bfb0a049f4e0d
Chirag Sharma <10406chiragsharma@gmail.com>
2025-04-11 17:23:50 +0530

    header setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
+import './Style/Spline.css'
+import './Style/common.css'
+import './Style/Page.css'
+
 import SplineComp from './components/Spline/Spline'
+import Page from './components/Pages/Page';
+import { JumpscareProvider } from './context/Jumpcontext';
+import { SplineProvider } from './context/splinecontext';
 
 function App() {
   return (
+    <JumpscareProvider>
+      <SplineProvider>
+        <div className='App'>
+          <div className='SplineContainer' >
+            <SplineComp />
+          </div>
+          <div className='PageContainer' >
 
-    <SplineComp/>
+            <Page />
 
+          </div>
+        </div>
+      </SplineProvider>
+    </JumpscareProvider>
   );
 }
 
---
+++ client/src/components/Pages/Page.jsx
@@ -0,0 +1,16 @@
+import React, { useState, useEffect } from "react";
+import Header from "../common/Header";
+
+function Page() {
+
+
+
+    return (
+        <div className="Page" style={{display:"flex",alignContent:"center",justifyContent:"center", zIndex:100}}>
+           <Header/>
+        </div>
+    );
+}
+
+
+export default Page;
